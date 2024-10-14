36018e591baf95c24787f5843381f659a8ecd95d
Utkarsh <utkarshgupta0122@gmail.com>
2024-10-13 22:57:00 -0400

    Made changes to MongoQuery

--- mongoQuery.py
+++ mongoQuery.py
@@ -1,12 +1,17 @@
 from pymongo import MongoClient
 import streamlit as st
 
-client = MongoClient(st.secrets["mongo"]['pymongo_string'])
+@st.cache_resource
+def init_connection():
+    return MongoClient(st.secrets["mongo"]['pymongo_string'])
+
+client = init_connection()
 db = client['5e-data'] 
 classes = db['classes']
 races = db['races']
 displayInfo = db['displayInfo']
 
+@st.cache_data(ttl=600)
 def fetchDisplayInfo(field):
     response = displayInfo.find_one({"field":field},{"_id":0, "options":1})
 
